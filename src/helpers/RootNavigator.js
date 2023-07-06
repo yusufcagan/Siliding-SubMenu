98ee7b2f397bf45eb8e35c2a7f52315e9d8e324a
Yusuf Çağan <yusufcagan@outlook.com.tr>
2023-07-06 18:52:39 +0300

    root fix

--- src/helpers/RootNavigator.js
+++ src/helpers/RootNavigator.js
@@ -1,4 +1,5 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React, { useEffect, useRef } from 'react'
 import Home from '../screens/home/Home'
 import Social from '../screens/social/Social'
 import Bulten from '../screens/bulten/Bulten'
@@ -8,9 +9,50 @@ import HomeIcon from '../assets/icons/home.png'
 import SocialIcon from '../assets/icons/social.png'
 import Ticket from '../assets/icons/ticket.png'
 import Finger from '../assets/icons/little_finger.png'
-import { Image, Text, View } from 'react-native';
+import { Image, Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+import * as Animatable from 'react-native-animatable';
 
 const Tab = createBottomTabNavigator();
+
+const BottomArr =
+    [
+        { route: 'Home', label: 'Home', src: HomeIcon, activeIcon: 'grid', inActiveIcon: 'grid-outline', component: Home },
+        { route: 'Social', label: 'Social', src: SocialIcon, activeIcon: 'grid', inActiveIcon: 'grid-outline', component: Social },
+        { route: 'Bulten', label: 'Bulten', src: Finger, activeIcon: 'grid', inActiveIcon: 'grid-outline', component: Bulten },
+        { route: 'Ratio', label: '2.4', src: HomeIcon, activeIcon: 'grid', inActiveIcon: 'grid-outline', component: Coupon },
+        { route: 'Coupon', label: 'Coupon', src: Ticket, activeIcon: 'grid', inActiveIcon: 'grid-outline', component: Ratio },
+    ]
+
+const TabButton = (props) => {
+    const { item, onPress, accessibilityState } = props;
+    const focused = accessibilityState.selected;
+    const viewRef = useRef(null);
+
+    useEffect(() => {
+        if (focused) {
+            viewRef.current.animate({ 0: { scale: .5, rotate: '0deg' }, 1: { scale: 1.5, rotate: '360deg' } });
+        } else {
+            viewRef.current.animate({ 0: { scale: 1.5, rotate: '360deg' }, 1: { scale: 1, rotate: '0deg' } });
+        }
+    }, [focused])
+
+    return (
+        <TouchableOpacity
+            onPress={onPress}
+            activeOpacity={1}
+            style={styles.container}>
+            <Animatable.View
+                ref={viewRef}
+                duration={1000}
+                style={styles.container}>
+                <Image
+                    source={item.src}
+                    name={focused ? item.activeIcon : item.inActiveIcon} color={focused ? 'blue' : 'grey'}
+                    style={{ height: 25, width: 25, tintColor: focused ? 'blue' : 'grey' }} />
+            </Animatable.View>
+        </TouchableOpacity>
+    )
+}
 
 function MyTabs() {
     return (
@@ -21,7 +63,17 @@ function MyTabs() {
                     backgroundColor: '#303144'
                 }
             })}>
-            <Tab.Screen name="Home" component={Home} options={{
+            {BottomArr.map((item, index) => {
+                return (
+                    <Tab.Screen key={index} name={item.route} component={item.component}
+                        options={{
+                            tabBarShowLabel: false,
+                            tabBarButton: (props) => <TabButton {...props} item={item} />
+                        }}
+                    />
+                )
+            })}
+            {/* <Tab.Screen name="Home" component={Home} options={{
                 tabBarIcon: ({ focused }) => (
                     <Image
                         source={HomeIcon}
@@ -59,9 +111,16 @@ function MyTabs() {
                         style={{ height: '70%', width: '30%', tintColor: focused ? 'blue' : 'grey' }}
                     />
                 )
-            }} />
+            }} /> */}
         </Tab.Navigator>
     );
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
 
 export default MyTabs;
